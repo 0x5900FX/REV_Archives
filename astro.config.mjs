1609bdff9b0f10a530d70eb4df41de80f96f34d2
0x5900FX <404niraj@gmail.com>
2026-06-06 23:16:17 +0545

    addded depn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   site: "https://www.cubeyond.net/",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
